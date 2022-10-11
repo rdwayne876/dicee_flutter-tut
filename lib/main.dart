7b79f342d4ec2bc5c5140eefbfd5acece3d073c7
rdwayne876 <63379431+rdwayne876@users.noreply.github.com>
2022-10-11 13:26:48 -0500

    update state on dice pressed

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   return runApp(
@@ -15,9 +16,55 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+
+  @override
+  State<DicePage> createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+
+  int leftDice = 6;
+  int rightDice = 6;
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: TextButton(
+                    onPressed: () {
+                      setState(() {
+                        leftDice = Random().nextInt(6)+1;
+                        rightDice = Random().nextInt(6)+1;
+                      });
+                    },
+                  child: Image(image: AssetImage('images/dice$leftDice.png')
+                  ),
+                ),
+              )
+          ),
+          Expanded(
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: TextButton(
+                  child: Image(image: AssetImage('images/dice$rightDice.png')
+                  ),
+                  onPressed: () {
+                    setState(() {
+                      leftDice = Random().nextInt(6)+1;
+                      rightDice = Random().nextInt(6)+1;
+                    });
+                  },
+                ),
+              )
+          ),
+        ],
+      ),
+    );
   }
 }
+
